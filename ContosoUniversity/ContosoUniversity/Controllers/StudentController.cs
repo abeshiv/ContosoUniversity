4d8b093404802d2d135d231f98bbcb07e26744ac
abeshiv <van.abeshi@gmail.com>
2014-07-03 11:20:10 -0400

    Implemented Request Logging(Students)

--- ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
 using PagedList;
@@ -102,7 +103,7 @@ namespace ContosoUniversity.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //line used for optional error log using above dex variable.
                 ModelState.AddModelError("", "Unable to save changes.");
@@ -141,7 +142,7 @@ namespace ContosoUniversity.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //log ze error
                 ModelState.AddModelError("", "Unable to save changes, contact admin.");
@@ -179,7 +180,7 @@ namespace ContosoUniversity.Controllers
                 db.Students.Remove(student);
                 db.SaveChanges();
             }
-            catch (DataException/* dex */)
+            catch (RetryLimitExceededException/* dex */)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.
                 return RedirectToAction("Delete", new { id = id, saveChangesError = true });
