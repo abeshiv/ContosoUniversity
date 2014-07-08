b028f69451b3b8a8bb5c03a6f29ba71f6e248cab
abeshiv <van.abeshi@gmail.com>
2014-07-08 11:56:11 -0400

    Implementing Inheritance Complete

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -9,6 +9,7 @@ namespace ContosoUniversity.DAL
         public DbSet<Course> Courses { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Person> People { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
